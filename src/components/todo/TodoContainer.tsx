0f308f2fa3da61a950c77014293148e529386b4c
1ibrahimtsc <1ibrahimtsc@gmail.com>
2024-03-09 19:59:53 +0600

    shadcn dialog

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,16 +1,38 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add todo</button>
+      <div className="flex justify-between">
+        <AddTodoModal />
         <button>Filter</button>
       </div>
-      <div className="bg-red-500 w-full h-[500px] rounded-xl p-5">
-        <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center">
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-3">
+        <div className="bg-white p-5 w-full h-full rounded-lg">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+        {/*
+<div className="bg-white p-5 text-2xl font-bold flex justify-center items-center">
           <p>There is no task pending</p>
         </div>
+          */}
       </div>
     </div>
   );
