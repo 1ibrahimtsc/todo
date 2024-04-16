9f8beb01c82d4e2cbd65b104d90838618593e8c3
1ibrahimtsc <1ibrahimtsc@gmail.com>
2024-04-16 07:44:42 +0600

    Add Todo Using Mutation Hook

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -13,27 +13,54 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
 import { addTodo } from "@/redux/features/todo/todoSlice";
+import { useAddTodosMutation } from "@/redux/api/api";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const AddTodoModal = () => {
   const [task, setTask] = useState("");
   const [description, setDescription] = useState("");
-  const dispatch = useAppDispatch();
+  const [priority, setPriority] = useState("");
+
+  console.log(priority);
+  //! For local state management
+  // const dispatch = useAppDispatch();
+
+  //* For server
+  //? [actualFunctionForPOst, {data, isLoading, isError, isSuccess} ]
+  const [addTodo, { data, isLoading, isError, isSuccess }] =
+    useAddTodosMutation();
+
+  console.log({ data, isLoading, isError, isSuccess });
   const { todos } = useAppSelector((state) => state.todos);
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     // console.log({ task, description });
 
-    const randomString = Math.random().toString(36).substring(2, 7);
+    // const randomString = Math.random().toString(36).substring(2, 7);
 
     const taskDetails = {
-      id: randomString,
       title: task,
-      description: description,
+      description,
+      isCompleted: false,
+
+      priority,
     };
 
     console.log(taskDetails);
-    dispatch(addTodo(taskDetails));
+    //! For local state management
+    // dispatch(addTodo(taskDetails));
+
+    //* For server
+    addTodo(taskDetails);
   };
   return (
     <Dialog>
@@ -71,6 +98,23 @@ const AddTodoModal = () => {
                 id="description"
                 className="col-span-3"
               />
+            </div>
+
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label className="text-right">Priority</Label>
+              <Select onValueChange={(value) => setPriority(value)}>
+                <SelectTrigger className="col-span-3">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Priority</SelectLabel>
+                    <SelectItem value="high">High</SelectItem>
+                    <SelectItem value="medium">Medium</SelectItem>
+                    <SelectItem value="low">Low</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
           </div>
           <div className="flex justify-end">
